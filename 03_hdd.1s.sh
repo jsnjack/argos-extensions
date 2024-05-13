1a1dd13bbc8034ed6a87e4c2da6618dcae2ad745
Yauhen Shulitski <jsnjack@gmail.com>
2024-05-13 11:51:41 +0200

    Customize hdd per system

--- 03_hdd.1s.sh
+++ 03_hdd.1s.sh
@@ -7,7 +7,12 @@ function padded {
     echo "$str" | awk '{printf "%*s", '$len', $1}'
 }
 
-hdd_usage=$(iostat -dxky 1 1 | kazy -i nvme0n1 | kazy -r -x "[\d.]*$")
+active_hdd=nvme0n1
+if [[ $HOSTNAME == "cm" ]]; then
+    active_hdd=nvme1n1
+fi
+
+hdd_usage=$(iostat -dxky 1 1 | kazy -i $active_hdd | kazy -r -x "[\d.]*$")
 rounded_hdd_usage=$(printf "%.0f" $hdd_usage)
 padded_hdd_usage=$(padded $rounded_hdd_usage 3)
 echo "io $padded_hdd_usage% | size=9px font=monospace"
