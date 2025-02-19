091d0d2ece6b641b08cc6a88c9da2f18c7801cbe
Yauhen Shulitski <jsnjack@gmail.com>
2025-02-19 16:12:54 +0100

    Update nordvpn endpoint url

--- 30_nordvpn.5s.sh
+++ 30_nordvpn.5s.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-response=$(curl -s -m 3 -w "\n%{http_code}" https://nordvpn.com/wp-admin/admin-ajax.php?action=get_user_info_data)
+response=$(curl -s -m 3 -w "\n%{http_code}" https://web-api.nordvpn.com/v1/ips/info)
 status_code="$(echo -e "$response" | tail -n 1)"
 response_text="$(echo -e "$response" | head -n -1)"
-vpn_status="$(echo -e "$response_text" | jq -r '.status')"
+vpn_status="$(echo -e "$response_text" | jq -r '.protected')"
 country_code="$(echo -e "$response_text" | jq -r '.country_code')"
 if [ "$status_code" = "200" ]; then
     if [ "$vpn_status" = "true" ]; then
